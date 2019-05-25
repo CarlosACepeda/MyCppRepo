776b8f4b2fb1f1e283da684fb32799363347aba1
Carlos Cepeda <carlosalt5126@hotmail.es>
2019-05-24 23:46:30 -0500

    Unary Operator '++' For loop

--- C++Test/HelloWorld.cpp
+++ C++Test/HelloWorld.cpp
@@ -8,5 +8,18 @@ int main()
 	string result;
 	cout << "Hello World";
 	cin >> result;
+
+	for (int i = 0; i < 9; i++)
+	{
+		cout << i;
+	};
+
+	int x = 1;
+	cout << x++; //Outputs 1;
+	cout << ++x; //Outputs 3;
+
+	cin >> result;
 	return 0;
+
+	//bitwise values.
 }
