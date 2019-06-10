2a9a5ad809cf2b0e881cc67d272afb94e23a122d
Carlos Cepeda <carlosalt5126@hotmail.es>
2019-06-09 22:33:54 -0500

    Octal system, Hexadecimal system, using bitwise operator: NOT.

--- C++Test/HelloWorld.cpp
+++ C++Test/HelloWorld.cpp
@@ -19,7 +19,29 @@ int main()
 	cout << ++x; //Outputs 3;
 
 	cin >> result;
+
+	//Ancient inherited from C octal system:
+	//by default c++ interprets numbers starting with 0 as octals.
+
+	cout << " 0173= " << 0173 << endl; //outputs 123.
+
+
+	//hexadecimal numbers
+	cout << "0x7B  = " << 0x7B << endl; //outputs 123 too
+	cout << "0x123 = " << 0x123 << endl; //outputs 291.
+
+
+	//Bitwise operators:
+	//~ :NOT, toggles the bit from 1 to 0 and 0 to 1.
+	//& :AND each bit of the left - hand argument with that on the right 
+	//| :OR each bit of the left - hand argument with that on the right 
+	//^ :XOR(exclusive OR) each bit of the left - hand argument with that on the right
+
+	int number6 = 0x6;
+	int number = ~number6;
+	cout << "This is 6, right? " << number; //outputs -7
+
 	return 0;
 
-	//bitwise values.
+	//DEFINE ALL the OTHER OPERATIONS. TODO
 }
