4e086380adc1e9d26f51bac649c6772590cde389
Carlos Cepeda <carlosalt5126@hotmail.es>
2020-01-13 23:05:07 -0500

    Organizing code and adding Arrays and While statements, with a nice Buffer Overrun

--- C++Test/HelloWorld.cpp
+++ C++Test/HelloWorld.cpp
@@ -8,20 +8,15 @@ using namespace std;
 int main()
 {
 	string result;
-	cout << "Hello World";
-	cin >> result;
-
+	cout << "Hello World" << endl;
 
 	//Limits:
 	INT_MAX;
 	INT_MIN;
 	//etc...
 
+	cout << "Unary addition Demo: ";
 
-	for (int i = 0; i < 9; i++)
-	{
-		cout << i << "Pollito" <<endl;
-	};
 	int x = 1;
 
 	cout << "X equals now to: " << x << endl;
@@ -30,21 +25,22 @@ int main()
 	cout << "++x: " << ++x << endl; //Outputs 3; //it makes the operation first then it returns the value.
 	cout << "X equals now to: " << x << endl;
 
-
+	cout << "Insert a string here: ";
 	cin >> result;
 
-	cout << "The size of result is " << sizeof(result)
-		<< " and the size of X is " << sizeof(x) << endl;
+	cout << "Sizeof Demo: (outputs the bytes used by certain datatype or variable datatype, either works)";
+	cout << "The size of 'result' is " << sizeof(result)
+		<< " and the size of 'x' is " << sizeof(x) << endl;
 
 	//Ancient inherited from C octal system:
 	//by default c++ interprets numbers starting with 0 as octals.
 
-	cout << " 0173= " << 0173 << endl; //outputs 123.
+	cout << " 0173 (Octal)= " << 0173 << endl; //outputs 123.
 
 
 	//hexadecimal numbers
-	cout << "0x7B  = " << 0x7B << endl; //outputs 123 too
-	cout << "0x123 = " << 0x123 << endl; //outputs 291.
+	cout << "0x7B  (Hexadecimal) = " << 0x7B << endl; //outputs 123 too
+	cout << "0x123 (Hexadecimal) = " << 0x123 << endl; //outputs 291.
 
 
 	//Bitwise operators:
@@ -55,14 +51,15 @@ int main()
 
 	int number6 = 0x6;
 	int number = ~number6;
-	cout << "This is 6, right? " << number << endl; //outputs -7
+	cout << "6 (hex) NOT operation applied: " << number << endl; //outputs -7
 	number = 2 | number6;
-	cout << "2 or 6 Equals to: " << number << endl; //outputs 6 
+	cout << "2 OR 6 Equals to: " << number << endl; //outputs 6 
 	number = 2 ^ number6;
-	cout << "2 xor 6 Equals to: " << number << endl; //outputs 4
+	cout << "2 Exclusive OR (XOR) 6 Equals to: " << number << endl; //outputs 4
 
 	//For Demo.
 		// input the loop count
+	cout << "For Demo:" << endl;
 	int nLoopCount;    cout << "Enter loop count: ";
 	cin  >> nLoopCount;    // count up to the loop count limit
 	for (int i = 1; i <= nLoopCount; i++)    
@@ -70,6 +67,27 @@ int main()
 		cout << "We've finished " << i 
 			<< " loops" << endl;   
 	}    
+
+	//Arrays. (array type of double)
+	//Foreach tipe there's an array.
+	//e.g. int myArray[3]; 
+	double productPrices[4]; 
+
+	auto index = 0;
+	cout << "While Demo:" << endl;
+	while (index < 7) //Buffer overrun because productPrices size is 4.
+	{
+		cout << "Enter price for product " << index << ":" << endl;
+		cin >> productPrices[index]; //at the moment we cross the last position (4) it writes to random memory places.
+		//basically outofbounds, unlike c# for example, it lets us to run the program and work 'perfectly'
+		//in Visual c++ it generates an exception.
+		//but in other compilers or platforms it may pass undetected. ;)
+		index++;
+	}
+
+
+
+
 	// wait until user is ready before terminating program
 	// to allow the user to see the program results    
 	cout << "Press Enter to continue..." << endl;
