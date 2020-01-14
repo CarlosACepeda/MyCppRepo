9e94d11f63e9b332c10f4d3751ca8b134973ad8b
Carlos Cepeda <carlosalt5126@hotmail.es>
2020-01-14 02:14:00 -0500

    Homework 1: The vending machine! Read code for full description of the homework.
    Alongside with some refactoring to previous code to allow better reading.

--- C++Test/HelloWorld.cpp
+++ C++Test/HelloWorld.cpp
@@ -68,24 +68,133 @@ int main()
 			<< " loops" << endl;   
 	}    
 
-	//Arrays. (array type of double)
+	//Arrays. (array type of float)
 	//Foreach tipe there's an array.
 	//e.g. int myArray[3]; 
-	double productPrices[4]; 
+	float productPrices[4];
 
-	auto index = 0;
-	cout << "While Demo:" << endl;
-	while (index < 7) //Buffer overrun because productPrices size is 4.
+	string crashOption = "n";
+	cout << "Do you wish to see a While loop demo that crashes the program?, If you do, press 'y' otherwise press 'n'" << endl;
+	cin >> crashOption;
+
+	if (crashOption== "y")
 	{
-		cout << "Enter price for product " << index << ":" << endl;
-		cin >> productPrices[index]; //at the moment we cross the last position (4) it writes to random memory places.
-		//basically outofbounds, unlike c# for example, it lets us to run the program and work 'perfectly'
-		//in Visual c++ it generates an exception.
-		//but in other compilers or platforms it may pass undetected. ;)
-		index++;
+		auto index = 0;
+		cout << "While Demo:" << endl;
+		while (index < 7) //Buffer overrun because productPrices size is 4.
+		{
+			cout << "Enter price for product " << index << ":" << endl;
+			cin >> productPrices[index]; //at the moment we cross the last position (4) it writes to random memory places.
+			//basically outofbounds, unlike c# for example, it lets us to run the program and work 'perfectly'
+			//in Visual c++ it generates an exception.
+			//but in other compilers or platforms it may pass undetected. ;)
+			index++;
+		}
 	}
 
+	//Homework 1: The vending machine.
+	/*
+	Ask the user how much money do they have
+	There must be at least 4 products.
+	The user will select a product from the product list
+	The program will inform the user what item they purchased and how much money they have left.
+	The program will continue to execute until the user has no money or decides to exit.
 
+	Allowed to use:
+	while loops.
+	if statements.
+	cin/cout.
+	arrays.
+	any primitive datatype.
+	variables.
+	*/
+
+	float clientMoney = 0;
+	string userchoice;
+	bool userHasFinishedBuying = false;
+	int userchosenProduct = -1;
+	bool userhasMoneyToPay = true;
+
+	string productsNames[4];
+	productsNames[0] = "Paper Aircraft";
+	productsNames[1] = "Paper Boat";
+	productsNames[2] = "Paper Rocket";
+	productsNames[3] = "Paper Super Fast Car";
+
+	productPrices[0] = 200.5f;
+	productPrices[1] = 105.6f;
+	productPrices[2] = 402.7f;
+	productPrices[3] = 105.5f;
+	
+
+	cout << "Welcome, Mr. Client, how much money do you have?: " << endl;
+	cin >> clientMoney;
+
+	if (clientMoney > 0)
+	{
+		while (userHasFinishedBuying==false)
+		{			
+			if(userhasMoneyToPay)
+			{
+				cout << "Products" << endl;
+				cout << "1." << productsNames[0] << " price: " << productPrices[0] << endl;
+				cout << "2." << productsNames[1] << " price: " << productPrices[1] << endl;
+				cout << "3." << productsNames[2] << " price: " << productPrices[2] << endl;
+				cout << "4." << productsNames[3] << " price: " << productPrices[3] << endl;
+
+				cout << "Please, enter the number of the product you'd wish to buy!" << endl;
+				cin >> userchosenProduct;
+
+				if (userchosenProduct != -1)
+				{
+					//let's check if the client has enough money to buy in the first place.
+					if (clientMoney > productPrices[userchosenProduct - 1])
+					{
+						userhasMoneyToPay = true;
+						clientMoney = clientMoney - productPrices[userchosenProduct - 1];
+						cout << "Okay Mr. Client, you bought " << productsNames[userchosenProduct - 1] << "!" << endl;
+
+						cout << "Your balance is now: " << clientMoney << endl;
+						cout << "Do you wish to buy another item? y/n" << endl;
+						cin >> userchoice;
+						if (userchoice == "y")
+						{
+							userHasFinishedBuying = false;
+						}
+						else if (userchoice == "n")
+						{
+							userHasFinishedBuying = true;
+						}
+						else
+						{
+							cout << "woah";
+						}
+					}
+					else
+					{
+						userhasMoneyToPay = false;
+						userHasFinishedBuying = true; //she does not have more money to buy things.
+						cout << "Come back when you have more money, those pennies won't afford our luxuries" << endl;
+					}
+				}
+			}
+			else
+			{
+				//automatically we say that the user has finished buying because she finished it or
+				//she doesn't have money to pay anyway, lol.
+				userHasFinishedBuying == true; //(For some reason this assignment isn't working LOL.
+				//The while loop executes forever, the debugger just skips this, to see this happening, remove the line
+				//175: "(userHasFinishedBuying = true; //she does not have more money to buy things.)".
+				cout << "You don't have more money left or you canceled the buy, thanks for coming" << endl;
+				
+			}
+		}
+		cout << "Thanks for buying, now you have " << clientMoney << " left!" << endl;
+	}
+	else
+	{
+		cout << "Who are you trying to fool?" << endl;
+	}
 
 
 	// wait until user is ready before terminating program
