f4b21a32c3aa230e1617eefad64108c87147d591
Carlos Cepeda <carlosalt5126@hotmail.es>
2020-03-16 16:02:58 -0500

    Adding additional comments learned in the class.

--- C++Test/VendingMachine.h
+++ C++Test/VendingMachine.h
@@ -5,7 +5,11 @@
 
 using namespace std;
 
-namespace //Anonymous namespace for private fields declaration
+namespace //Anonymous namespace for private fields declaration only accesible within the cpp file.
+	//Or in this case only on the VendingMachine.cpp
+	//or in the cpp file that referenced this header!
+	//Though putting anonymous members in the header maybe useless.
+	//If this header gets referenced in another cpp file it won't compile LOL
 {
 	float clientMoney = 0;
 	string userchoice;
@@ -14,6 +18,8 @@ namespace //Anonymous namespace for private fields declaration
 	bool userHasMoneyToPay = true; //By default the user has money to pay.
 	string productNames[4];
 	float productPrices[4] = {-1};
+	//extern bool myVariable ---> prototyping a variable and promising the compiler this variable exists somewhere
+	//same with functions I have to provide a implementation of this variable at certain point
 }
 
 //Declare prototypes. (I'll use also a custom namespace)
