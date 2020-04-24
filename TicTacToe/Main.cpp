d66c7b5619df745d48f2e1a90e27dceb95276248
Carlos Cepeda <carlosalt5126@hotmail.es>
2020-04-23 22:24:19 -0500

    Homework 7: Create the engine that allows a player to win the game. Now it actually works flawlessly, damn my first game in C++!

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -70,6 +70,14 @@ public:
 	}
 	Winner::E Run() 
 	{
+		//Validity check.
+		//if the board is less than 3 squares width
+		if (_board.GetWidth() < 3)
+		{
+			std::cout << "Invalid quantity of board, use 3*3 or higher!\n";
+			return Winner::None;
+		}
+
 		Winner::E winningPlayer, currentPlayer = Winner::X;
 		while ((winningPlayer= _ruleEngine.GetWinningPlayer(_board))== Winner::None)
 		{
@@ -88,6 +96,7 @@ public:
 			_board.SetSquare(input, currentPlayer== BoardSquare::X? BoardSquare::X: BoardSquare::O);
 			currentPlayer = currentPlayer == Winner::X ? Winner::O : Winner::X;
 		}
+		std::cout << GetWinnerText(winningPlayer);
 		return winningPlayer;
 	}
 private:
@@ -115,6 +124,27 @@ private:
 		}
 		return index + '0';
 	}
+	std::string GetWinnerText(Winner::E name)
+	{
+		switch (name)
+		{
+		case Winner::X:
+			return "X wins\n";
+			break;
+		case Winner::O:
+			return "O wins\n";
+			break;
+		case Winner::Stalemate:
+			return "It's a tie\n";
+			break;
+		case Winner::None:
+			return "Nobody wins?? How did you do that\n";
+			break;
+		default:
+			return "???\n";
+			break;
+		}
+	}
 };
 class NullRuleEngine : public IRuleEngine //public class
 {
@@ -124,18 +154,136 @@ public:
 		return Winner::None;
 	}
 };
+
+//Homework 7:
+//Create the engine that allows a player to win the game!
+//Works flawlessly! ;)
+class ClassicRuleEngine : public IRuleEngine
+{
+
+public:
+	virtual Winner::E GetWinningPlayer(Board& board)
+	{
+		bool linematches = false; //Identify if there's 3 in a row.
+		int samePlayerCount = 1; //Keep a count of the current matches of a player in the board.
+		Winner::E winner;
+		if (linematches == false)
+		{
+			samePlayerCount = 1;
+			for (auto i = 0; i < board.GetTotalSquares(); i++)
+			{
+				if (board.GetSquare(i) == BoardSquare::Empty)
+				continue; //move to the next item.
+
+				   //Horizontal check.
+				if (i == 0) continue; //greater than or equals because the next line will make a check with the next item.
+				if ((i + 1) % board.GetWidth() == 0) //The only one that equals 0 is 0 and the Width of the board,
+						//this way we know we are int the last item horizontally if this is true.
+					{
+						for (int reverseindex = i; reverseindex > (i - 2); reverseindex--) //now lets read from the last to the first horizontally.
+						{
+							if ((reverseindex - 1) < 0) continue;
+
+							if (board.GetSquare(reverseindex) == board.GetSquare(reverseindex - 1))
+							{
+								samePlayerCount = samePlayerCount + 1;
+							}
+							if (samePlayerCount == board.GetWidth())
+							{
+								linematches = true;
+								winner = (board.GetSquare(reverseindex) == BoardSquare::X) ? Winner::X : Winner::O;
+								break;
+							}
+						}
+					}
+				
+				if (linematches) break; //Exit of the main loop because I already found a Winner!
+			}
+		}
+		if (linematches == false)
+			{
+				//Still no match? check vertically.
+				samePlayerCount = 1; //reset.
+				for (auto i = 0; i < board.GetTotalSquares(); i++)
+				{
+					if (board.GetSquare(i) == BoardSquare::Empty) continue; //move to the next item.
+					if (board.GetSquare(i) == board.GetSquare(i + board.GetWidth()))
+					{
+						samePlayerCount = samePlayerCount + 1;
+						i += board.GetWidth() - 1;
+					}
+					if (samePlayerCount == board.GetWidth())
+					{
+						linematches = true;
+						winner = (board.GetSquare(i + 1) == BoardSquare::X) ? Winner::X : Winner::O;
+						break;
+
+					}
+				}
+			}
+		if (linematches == false)
+			{
+				//If still does not matchhhhhh then let's prove now in diagonal fashion
+				//there are two diagonals
+				samePlayerCount = 1; //reset again.
+				//from Top Left:
+				for (auto i = 0; i < board.GetTotalSquares(); i++)
+				{
+					if (board.GetSquare(0) == BoardSquare::Empty) break; //if the top left is empty then don't check for anything.
+					//first diagonal (starting from top left)
+					if (board.GetSquare(i) == board.GetSquare(i + board.GetWidth() + 1))
+					{
+						samePlayerCount = samePlayerCount + 1;
+						i += board.GetWidth();
+					}
+					if (samePlayerCount == board.GetWidth())
+					{
+						linematches = true;
+						winner = (board.GetSquare(i + 1) == BoardSquare::X) ? Winner::X : Winner::O;
+						break;
+					}
+				}
+				samePlayerCount = 1; //reset again.
+				//from Top Right:
+				for (auto i = (board.GetWidth()-1); i < board.GetTotalSquares(); i++)
+				{
+					if (board.GetSquare(i) == BoardSquare::Empty) break; //if the top right is empty then don't check for anything.
+					if (board.GetSquare(i) == board.GetSquare(i + board.GetWidth() - 1))
+					{
+						samePlayerCount = samePlayerCount + 1;
+						i += board.GetWidth() - 2;
+					}
+					if (samePlayerCount == board.GetWidth())
+					{
+						linematches = true;
+						winner = (board.GetSquare(i-1) == BoardSquare::X) ? Winner::X : Winner::O;
+						break;
+					}
+				}
+			}
+
+		if (linematches)
+		{
+			return winner;
+		}
+		else
+		{
+			return Winner::None;
+		}
+		
+	}
+};
 int main()
 {
+
 	Board board(3); //Constructor on stack.
 
-	NullRuleEngine nullruleEngine;
+	ClassicRuleEngine classicRuleEngine;
 
-	Game game(board, nullruleEngine);
+	Game game(board, classicRuleEngine);
 	game.Run();
 
-	//Homework 7:
-	//Create the engine that allows a player to win the game.
-	//right now it's impossible to win the game.
+	
 
 	return 0;
 }
